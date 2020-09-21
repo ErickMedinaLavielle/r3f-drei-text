72fee22c4b96331673a43429a4f9dd1a2591dd91
ErickMedinaLavielle <58129138+ErickMedinaLavielle@users.noreply.github.com>
2020-09-20 22:21:31 -0500

    adding fonts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Canvas } from "react-three-fiber";
 import { Text } from "drei";
+import font from "./fonts/PinyonScript-Regular.ttf";
 import "./styles.css";
 
 export default function App() {
   return (
-    <Canvas>
-      <Text
-        color={"#EC2D2D"}
-        fontSize={1}
-        maxWidth={200}
-        lineHeight={1}
-        letterSpacing={0.02}
-        textAlign={"top"}
-        font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-        anchorX="center"
-        anchorY="middle"
-      >
-        LOREM IPSUM.
-      </Text>
-    </Canvas>
+    <>
+      <Canvas>
+        <Text
+          color={"#EC2D2D"}
+          fontSize={1}
+          maxWidth={200}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={"top"}
+          font={font}
+          anchorX="center"
+          anchorY="middle"
+        >
+          Karina Gazal.
+        </Text>
+      </Canvas>
+    </>
   );
 }
